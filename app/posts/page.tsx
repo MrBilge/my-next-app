e4f8379d03e107ba8f928df3dedd95bff6c60c52
MrBilge <b.eybek9307@gmail.com>
2024-08-26 09:24:16 +0300

    new

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -5,22 +5,23 @@ export default async function Form() {
     const res = await fetch("https://jsonplaceholder.typicode.com/posts");
 
     if (!res.ok) {
-        throw new Error('Not found Post')
+      throw new Error("Not found Post");
     }
 
-     return res.json()
+    return res.json();
   };
-    
-    const posts = await getPostData() 
+
+  const posts = await getPostData();
 
   return (
-    <div  className="flex flex-col justify-center items-center m-5">
-  
-      {posts.map((post:any)=>(
-      <Link key={post.id} href={`posts/${post.id}`}>
-        <p>{post.title}</p>
-        </Link> 
+    <div className="flex flex-col justify-center items-center m-5">
+      {posts.map((post: any) => (
+        <Link key={post.id} href={`posts/${post.id}`}>
+          <p>{post.title}</p>
+        </Link>
       ))}
     </div>
   );
 }
+
+// Try
